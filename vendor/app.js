15f4a0af7612edcddba5c57ba678c8f4a7c497c9
Namwa <sawasdiworn.k@gmail.com>
2021-09-29 12:45:13 +0100

    Fix image bug
    Fix: the image preview of the same file didn't show up because the input value wasn't cleared.

--- vendor/app.js
+++ vendor/app.js
@@ -22,6 +22,7 @@ document.addEventListener('alpine:init', () => {
     removeLogo() {
       this.uploadSelected = false;
       this.chkLogo = false;
+      this.$refs.logoInput.value = '';
     }
   });
 });
