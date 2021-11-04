0e6c07d3b47fff811b8b7d7414a2f27d7197f05a
Namwa <sawasdiworn.k@gmail.com>
2021-11-04 17:03:52 +0000

    build.sh generates build directory with versioned zip file

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-zip -r theme manifest.json
-zip -r theme templates -i \*.html \*.txt
-zip -r theme public -i \*.html \*.css \*.woff2 \*.js \*.ico \*.png
+npm run --silent tw:prod
+
+# make the build directory and get the version from the manifest file
+# to generate the theme zip file path.
+mkdir -p build
+ZIPFILE=build/jobboard-default-theme-`cat manifest.json | jq .version | tr -d '"'`.zip
+
+zip -rq $ZIPFILE manifest.json
+zip -rq $ZIPFILE templates -i \*.html \*.txt
+zip -rq $ZIPFILE public -i \*.html \*.css \*.woff2 \*.js \*.ico \*.png
